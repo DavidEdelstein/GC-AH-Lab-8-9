4810132b2e08e5326d77a71f1e51234c189439cb
David Edelstein <David Edelstein>
2018-10-29 22:53:06 -0400

    moved arrays to lists

--- GC-AH Lab 8-9/Program.cs
+++ GC-AH Lab 8-9/Program.cs
@@ -13,29 +13,31 @@ namespace GC_AH_Lab_8_9
             while (true)
             {
 
-                string[] StudentNames = new string[6] { "DAVID", "SAM", "TOD", "JUBIS", "SATAN", "MOSES" };
-                string[] favoriteFood = new string[6] { "Cake", "Cookies", "Roast Beef", "Sushi", "Hellfire", "Sand" };
-                string[] homeTown = new string[6] { "Detroit", "Lansing", "Djbouti", "Oaugadougu", "Hell", "The Desert" };
-
-                Console.Write("Which student would you like to learn about? (Enter a number 1-6:");
+                List<string> StudentNames = new List<string> { "DAVID", "SAM", "TOD", "JUBIS", "SATAN", "MOSES" };
+                List<string> favoriteFood = new List<string> { "Cake", "Cookies", "Roast Beef", "Sushi", "Hellfire", "Sand" };
+                List<string> homeTown = new List<string> { "Detroit", "Lansing", "Djbouti", "Oaugadougu", "Hell", "The Desert" };
+                //homeTown.Add("foo");
+                //homeTown.Add(Console.ReadLine());
+                Console.Write("Would you like to learn about a student?(Enter a number 1-6) -OR- Would you like to add a student to the directory?(y/n):");
+                var add = Console.ReadLine();
                 if (int.TryParse(Console.ReadLine(), out int number))
                 {
                     Console.WriteLine($"Student #{number} is {StudentNames[number - 1]}");
                     Console.WriteLine("=============");
                     while (true)
                     {
-                        Console.WriteLine($"Do you want to know more about {StudentNames[number-1]}?");
+                        Console.WriteLine($"Do you want to know more about {StudentNames[number - 1]}?");
                         Console.Write("Type in 'Favorite Food' or 'Hometown'(Check for proper capitalization); To exit type 'exit':");
                         var answer = Console.ReadLine();
                         if (answer == "Favorite Food")
                         {
-                            Console.WriteLine($"{favoriteFood[number-1]} is {StudentNames[number-1]}'s favorite food.");
+                            Console.WriteLine($"{favoriteFood[number - 1]} is {StudentNames[number - 1]}'s favorite food.");
                         }
                         else if (answer == "Hometown")
                         {
-                            Console.WriteLine($"{StudentNames[number-1]}'s hometown is {homeTown[number - 1]}");
+                            Console.WriteLine($"{StudentNames[number - 1]}'s hometown is {homeTown[number - 1]}");
                         }
-                        else if(answer == "exit" )
+                        else if (answer == "exit")
                         {
                             return;
                         }
@@ -44,12 +46,16 @@ namespace GC_AH_Lab_8_9
                             Console.WriteLine("That is invalid--Either 1)Favorite Food 2)Hometown 3)exit, THANK YOU.");
                         }
                     }
-                    //Console.ReadKey();
                 }
-                else
+                else if (add != "y")
                 {
-                    Console.WriteLine("Only input numbers from 1-6.");
+                    Console.WriteLine("Please enter the students name:");
+                    StudentNames.Add(Console.ReadLine());
                 }
+                //else
+                //{
+                //    Console.WriteLine("Only input numbers from 1-6.");
+                //}
             }
         }
     }
